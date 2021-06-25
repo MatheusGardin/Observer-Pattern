5b673a6a8a04cf497e30f2151ea5ee4164d11126
MatheusGardin <jackmatheus123@gmail.com>
2021-06-25 18:45:13 -0300

    Teste concluído

--- observer.js
+++ observer.js
@@ -1,45 +1,42 @@
 // Definimos a classe
 class Subject {
-    // Cada instância da classe Observer
-    // começa com um array vazio de observadores
-    // que reagem a uma mudança de estado
+    // Cada instância da classe Observer começa com um array vazio de observadores que reagem a uma mudança de estado
     constructor() {
         this.observers = [];
     }
 
-    // Adcione a capacidade de inscrever um novo objeto/Elemento DOM
-    // essencialmente, adicone algo ao array de observadores
-    subscribe(f) {
-        this.observers.push(f);
+    // Adcione a capacidade de inscrever um novo objeto/Elemento DOM essencialmente, adicone algo ao array de observadores
+    subscribe(fn) {
+        this.observers.push(fn);
     }
 
-    // Adcione a capacidade de cancelar a inscrição de um objeto em particular
-    // essencialmente, remove algum item do array de observadores
-    unsubscribe(f) {
-        this.observers = this.observers.filter(subscriber => subscriber !== f);
+    // Adcione a capacidade de cancelar a inscrição de um objeto em particular essencialmente, remove algum item do array de observadores
+    unsubscribe(fnToRemove) {
+        this.observers = this.observers.filter(fn => {
+            if(fn !== fnToRemove)
+                return fn
+        });
     }
 
-    // Atualiza todos os objetos incritos/Elementos DOM
-    // e passa alguns dados para cada um deles
+    // Atualiza todos os objetos incritos/Elementos DOM e passa alguns dados para cada um deles
     fire() {
-        this.observers.forEach( f => {
-            f.call()
+        this.observers.forEach( fn => {
+            fn.call()
         })
     }
 }
 
 
-function ObserverTest(){
-    console.log("observer test works!")
+function observerTest(){
+    console.log(input.value);
 }
 
-function AnotherFunction(){
-    console.log("it works again")
-}
+const input = document.querySelector("#name");
 
 const subject = new Subject();
 
-subject.subscribe(ObserverTest);
-subject.subscribe(AnotherFunction);
+subject.subscribe(observerTest);
 
-subject.fire();
+input.addEventListener('keyup', e => {
+    subject.fire(e.target.value);
+});
